03d1cd47fbe95972714d25152ffb1a0040f84f24
kylie <kylie@penxle.io>
2026-02-05 17:03:39 +0900

    prettier 세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,26 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // Integrate Prettier with ESLint: run Prettier as an ESLint rule and disable conflicting rules
+  {
+    plugins: ['prettier'],
+    extends: ['plugin:prettier/recommended', 'prettier'],
+    rules: {
+      // Let prettier handle formatting
+      'prettier/prettier': 'error',
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
 ]);
 
