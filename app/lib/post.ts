8ed9f53df5c64a1e29782d7b3246bc5c0dce2b24
Seohyun Roh <radult951@gmail.com>
2026-05-02 14:53:27 +0900

    마크다운 GFM 표 지원 추가

--- app/lib/post.ts
+++ app/lib/post.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { remark } from 'remark';
+import gfm from 'remark-gfm';
 import html from 'remark-html';
 
 const postsDir = path.join(process.cwd(), 'blog');
@@ -50,7 +51,7 @@ export async function getPostBySlug(slug: string) {
 
   const raw = fs.readFileSync(fullPath, 'utf8');
   const { data, content } = matter(raw);
-  const processed = await remark().use(html).process(content);
+  const processed = await remark().use(gfm).use(html).process(content);
   const contentHtml = processed.toString();
 
   return {
